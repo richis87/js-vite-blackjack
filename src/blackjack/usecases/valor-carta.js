660f38840a5f806b64b5d6223fb25758b4a7866a
Richard Zuñiga <richardzu10@hotmail.com>
2024-04-25 16:07:54 -0600

    Primer commit

---
+++ src/blackjack/usecases/valor-carta.js
@@ -0,0 +1,12 @@
+// pedirCarta();
+/**
+ * Obtener el valor de la carta
+ * @param {string} carta 
+ * @returns {number} valor de la carta
+ */
+export const valorCarta = ( carta ) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ? 
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+}
